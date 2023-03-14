ef6783184833000f2351ccc581bb7da732591127
jananisriram <jananisriram7@gmail.com>
2023-03-13 20:00:45 -0700

    working on connecting client with server section

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';import logo from './logo.svg';import './App.scss';import socketClient  from "socket.io-client";
+const SERVER = "http://127.0.0.1:8080";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    var socket = socketClient (SERVER);
+
+    return (
+        <div classname="App">
+
+            <header classname="App-header"></header>
+
+                <img src="{logo}" classname="App-logo" alt="logo"></img>
+                <p></p>        
+                Edit <code>src/App.js</code> and save to reload.  
+               <p></p>‍
+               
+                   className="App-link"
+                   href="https://reactjs.org"
+                   target="_blank"
+                   rel="noopener noreferrer"
+                {/* > */} Learn React
+        </div>       
+    );
+‍
 }
 
 export default App;
